9493e351e83f096a1049164bcb92a4148d40a259
Damodar Lohani <dlohani48@gmail.com>
2022-07-11 09:28:34 +0000

    cli initialization

--- src/Platform.php
+++ src/Platform.php
@@ -2,24 +2,44 @@
 
 namespace Utopia\Platform;
 
+use Exception;
 use Utopia\App;
+use Utopia\CLI\CLI;
 
 abstract class Platform
 {
-    protected array $services = [];
+    protected array $services = [
+        'all' => [],
+        Service::TYPE_CLI => [],
+        Service::TYPE_HTTP => [],
+        Service::TYPE_GRAPHQL => []
+    ];
+
+    protected CLI $cli;
 
     /**
      * Initialize Application
      *
      * @return void
      */
-    public function init(): void
+    public function init(string $type): void
     {
-        foreach ($this->services as $key => $service) {
-            switch ($service->getType()) {
-                case Service::TYPE_HTTP:
-                    $this->initHttp($service);
-            }
+        switch($type){
+            case Service::TYPE_HTTP:
+                $this->initHttp();
+                break;
+            case Service::TYPE_CLI:
+                $this->initCLI();
+                break;
+            case Service::TYPE_GRAPHQL:
+                $this->initGraphQL();
+                break;
+            case 'all':
+            default:
+                $this->initHttp();
+                $this->initCli();
+                $this->initGraphQL();
+                break;
         }
     }
 
@@ -29,29 +49,67 @@ abstract class Platform
      * @param Service $service
      * @return void
      */
-    protected function initHttp(Service $service): void
+    protected function initHttp(): void
     {
-        foreach ($service->getActions() as $key => $action) {
-            /** @var Action $action */
-            $route = App::addRoute($action->getHttpMethod(), $action->getHttpPath());
-
-            $route->groups($action->getGroups());
-            $route->alias($action->getHttpAliasPath(), $action->getHttpAliasParams());
-
-            foreach ($action->getParams() as $key => $param) {
-                $route->param($key, $param['default'], $param['validator'], $param['description'], $param['optional'], $param['injections']);
+        foreach($this->services[Service::TYPE_HTTP] as $service) {
+            foreach ($service->getActions() as $action) {
+                /** @var Action $action */
+                $route = App::addRoute($action->getHttpMethod(), $action->getHttpPath());
+                $route
+                    ->groups($action->getGroups())
+                    ->alias($action->getHttpAliasPath(), $action->getHttpAliasParams());
+    
+                foreach ($action->getParams() as $key => $param) {
+                    $route->param($key, $param['default'], $param['validator'], $param['description'], $param['optional'], $param['injections']);
+                }
+    
+                foreach ($action->getInjections() as $injection) {
+                    $route->inject($injection);
+                }
+    
+                foreach ($action->getLabels() as $key => $label) {
+                    $route->label($key, $label);
+                }
+    
+                $route->action($action->getCallback());
             }
-
-            foreach ($action->getInjections() as $injection) {
-                $route->inject($injection);
-            }
-
-            foreach ($action->getLabels() as $key => $label) {
-                $route->label($key, $label);
-            }
-
-            $route->action($action->getCallback());
         }
+    }
+
+    /**
+     * Init CLI Services
+     *
+     * @return void
+     */
+    protected function initCLI(): void
+    {
+        $this->cli ??= new CLI();
+        foreach ($this->services[Service::TYPE_CLI] as $service) {
+            foreach ($service as $key => $action) {
+                $task = $this->cli->task($key);
+                $task
+                    ->desc($action->getDesc())
+                    ->action($action->getCallback());
+
+                foreach ($action->getParams() as $key => $param) {
+                    $task->param($key, $param['default'], $param['validator'], $param['description'], $param['optional'], $param['injections']);
+                }
+    
+                foreach ($action->getLabels() as $key => $label) {
+                    $task->label($key, $label);
+                }
+            }
+        }
+    }
+
+    /**
+     * Initialize GraphQL Services
+     *
+     * @return void
+     */
+    protected function initGraphQL(): void
+    {
+
     }
 
     /**
@@ -63,7 +121,8 @@ abstract class Platform
      */
     public function addService(string $key, Service $service): Platform
     {
-        $this->services[$key] = $service;
+        $this->services['all'][$key] = $service;
+        $this->services[$service->getType()] = $service;
         return $this;
     }
 
---
+++ src/Type.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Utopia\Platform;
+
+abstract class Type {
+    public const CLI = 'CLI';
+    public const HTTP = 'http';
+    public const GRAPH_QL = 'GraphQL';
+}
