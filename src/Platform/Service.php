dfcae35191d533395f39eda2f28e674025002559
Damodar Lohani <dlohani48@gmail.com>
2022-08-01 06:04:25 +0000

    make hooks part of service

--- src/Platform/Service.php
+++ src/Platform/Service.php
@@ -14,6 +14,10 @@ abstract class Service
 
     protected array $actions;
     protected string $type;
+    protected array $initHooks = [];
+    protected array $shutdownHooks = [];
+    protected array $errorHooks = [];
+    protected array $optionHooks = [];
 
     /**
      * Set Type
@@ -81,5 +85,68 @@ abstract class Service
     public function getActions(): array
     {
         return $this->actions;
+    }
+
+    /**
+     * Add Init Hook
+     *
+     * @param Action $hook
+     * @return void
+     */
+    public function addInitHook(Action $hook): static
+    {
+        $initHooks[] = $hook;
+        return $this;
+    }
+
+    /**
+     * Add Shutdown Hook
+     *
+     * @param Action $hook
+     * @return static
+     */
+    public function addShutdownHook(Action $hook): static
+    {
+        $shutdownHooks[] = $hook;
+        return $this;
+    }
+
+    /**
+     * Add Option Hook
+     *
+     * @param Action $hook
+     * @return static
+     */
+    public function addOptionHook(Action $hook): static
+    {
+        $optionHooks[] = $hook;
+        return $this;
+    }
+
+    /**
+     * Add Error Hook
+     *
+     * @param Action $hook
+     * @return static
+     */
+    public function addErrorHook(Action $hook): static
+    {
+        $errorHooks[] = $hook;
+        return $this;
+    }
+
+    /**
+     * Get Hooks
+     *
+     * @return array
+     */
+    public function getHooks(): array
+    {
+        return [
+            'init' => $this->initHooks,
+            'error' => $this->errorHooks,
+            'shutdown' => $this->shutdownHooks,
+            'option' => $this->optionHooks
+        ];
     }
 }
