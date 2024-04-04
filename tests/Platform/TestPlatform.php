d95460bee9c04febd1cbab6086ae0a67cb425f64
Damodar Lohani <dlohani48@gmail.com>
2024-04-04 07:36:50 +0000

    fix test

--- tests/Platform/TestPlatform.php
+++ tests/Platform/TestPlatform.php
@@ -3,11 +3,15 @@
 namespace Utopia\Tests;
 
 use Utopia\Platform\Platform;
+use Utopia\Platform\Module;
 
 class TestPlatform extends Platform
 {
     public function __construct()
     {
+        $module = new Module();
+        parent::__construct($module);
+
         $this->addService('testService', new TestService());
         $this->addService('testCli', new TestServiceCLI());
     }
