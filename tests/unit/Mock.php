ca652e88b9114ce8c179623fb9ae70219ba6b3d6
shimon <shimon@appwrite.io>
2024-04-25 20:42:48 +0300

    unit test

---
+++ tests/unit/Mock.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Utopia\Unit;
+use Utopia\Platform\Platform;
+use Utopia\Tests\TestModule;
+
+class Mock extends Platform
+{
+        public function __construct(){
+
+            $module = new TestModule();
+            parent::__construct($module);
+
+        }
+}
