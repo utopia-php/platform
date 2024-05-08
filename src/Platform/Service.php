5e10bf801137a747521a7b6aaccebda9f7e1e298
Shimon Newman <shimonewman@gmail.com>
2024-05-08 18:59:57 +0000

    composer format

--- src/Platform/Service.php
+++ src/Platform/Service.php
@@ -7,7 +7,7 @@ abstract class Service
     public const TYPE_HTTP = 'http';
 
     public const TYPE_GRAPHQL = 'GraphQL';
-    
+
     public const TYPE_TASK = 'Task';
 
     public const TYPE_CLI = 'CLI';
