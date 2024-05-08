e277e162e505ea44d5dc3f35a8b32f239dfed1f2
shimon <shimon@appwrite.io>
2024-05-08 19:51:13 +0300

    composer format

--- tests/unit/Mock.php
+++ tests/unit/Mock.php
@@ -1,15 +1,15 @@
 <?php
 
 namespace Utopia\Unit;
+
 use Utopia\Platform\Platform;
 use Utopia\Tests\TestModule;
 
 class Mock extends Platform
 {
-        public function __construct(){
-
-            $module = new TestModule();
-            parent::__construct($module);
-
-        }
+    public function __construct()
+    {
+        $module = new TestModule();
+        parent::__construct($module);
+    }
 }
